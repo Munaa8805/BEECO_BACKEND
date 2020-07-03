b5384d2ad7c77715d4928967c2f86809c906d4cd
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2020-07-03 18:16:36 +0800

    Build

---
+++ crud-app/build/precache-manifest.e041c54396d2c3d48f34b5569c4f67d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7820af94649d9eeb139606176c194df9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3175dec7a83e51190a3b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df33ea94775ceb476389",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3175dec7a83e51190a3b",
+    "url": "/static/js/2.c5b6b6e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c5b6b6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df33ea94775ceb476389",
+    "url": "/static/js/main.f32d6912.chunk.js"
+  },
+  {
+    "revision": "652a909d2c7614fc83c4",
+    "url": "/static/js/runtime-main.82c53c28.js"
+  }
+]);
