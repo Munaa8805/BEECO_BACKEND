573b71652efc7c501d433d4f4504fc332727fa3b
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2020-07-06 00:25:31 +0800

    string solib

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
+  const conn = await mongoose.connect(
+    "mongodb+srv://1234:1234@crudbeeco-odnco.mongodb.net/beeco?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+  );
   console.log(
     `MongoDB connected : ${conn.connection.host}`.cyan.underline.bold
   );
